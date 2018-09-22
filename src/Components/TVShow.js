54b08a681436d60b5d5a0b65c91182dafb5ae7e4
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-09-22 18:28:15 -0400

    Done.

--- src/Components/TVShow.js
+++ src/Components/TVShow.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const tvShow = () => {
+const TVShow = (props) => {
   return (
     <div>
       <br/>
-      <img src={props.image.medium} onClick={props.selectShow} alt=""/>
+      <img src={props.show.image.medium} 
+      onClick={(e) => {
+        e.preventDefault();
+        props.selectShow(props.show)}} alt=""/>
     </div>
   );
 }
 
-export default tvShow;
+export default TVShow;
